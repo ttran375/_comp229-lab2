93523e9d94926ef40fb8dd61371784a9092bdf63
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-06 18:40:05 -0400

    Exercise 3: Mounting Connect Middleware

--- lab-session-2/server.js
+++ lab-session-2/server.js
@@ -1,9 +1,24 @@
 const connect = require('connect');
 const app = connect();
+
+function logger(req, res, next) {
+    console.log(req.method, req.url);
+    next();
+}
+
 function helloWorld(req, res, next) {
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
-};
-app.use(helloWorld);
+}
+
+function goodbyeWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Goodbye World');
+}
+
+app.use(logger);
+app.use('/hello', helloWorld);
+app.use('/goodbye', goodbyeWorld);
+
 app.listen(3000);
 console.log('Server running at http://localhost:3000/');
