282da35238c7ce3f0be9360d2b9753cf872d0b17
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-06 22:27:44 +0000

    Exercise 1: Develop Node.js Web Application

---
+++ lab-session-2/server.js
@@ -0,0 +1,9 @@
+const connect = require('connect');
+const app = connect();
+function helloWorld(req, res, next) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+};
+app.use(helloWorld);
+app.listen(3000);
+console.log('Server running at http://localhost:3000/');
